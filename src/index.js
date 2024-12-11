6fd0e60801c500ad7b36aa9a00f4498a64a034b9
Deborah964 <fiankobeadeborah@gmail.com>
2024-12-11 11:24:59 +0000

    my homework5 file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 let name=prompt("type your name");
 let age=prompt("type your age");
 let email=prompt("type your email");
-alert('Hi {name}, youre {age} years old and we shall keep in touch via your email:{email}')
+alert(`Hi ${name}, youre ${age} years old and we shall keep in touch via your email:${email}`)
